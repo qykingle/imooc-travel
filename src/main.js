bc8230532ada97111f7aea1ca6ba071ea440324d
qykingle <qianyongkang12345@gmail.com>
2018-07-01 23:00:35 +0800

    add header

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 重置样式
+import 'styles/reset.css'
+// 移动端边框1px问题解决
+import 'styles/border.css'
+import 'styles/iconfont.css'
+// 移动端click三百毫秒延迟解决
+import fastClick from 'fastclick'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <div class="iconfont back-icon">&#xe624;</div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<!--scoped 保证css样式不会对其他文件产生影响-->
+<style scoped lang="stylus">
+  @import '~styles/varibles.styl'
+  //1rem = html font-size = 50px
+  .header
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      width: .64rem
+      float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      padding-left: .2rem
+      margin-top: .12rem
+      margin-left: .2rem
+      background: #ffffff
+      border-radius: .1rem
+      color: #ccc
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        font-size: .24rem
+</style>
