b140bacf5b4d0f4c9abecffbe8d499f46611289a
qykingle <qianyongkang12345@gmail.com>
2018-08-13 15:28:51 +0800

    banner finish

---
+++ src/common/gallary/Gallary.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="container" @click="handleGallaryClick ">
+    <div class="wrapper">
+      <swiper :options="swiperOptions">
+        <!-- slides -->
+        <swiper-slide>
+          <img src="http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg"
+               class="gallary-img">
+        </swiper-slide>
+        <swiper-slide>
+          <img src="http://img1.qunarzz.com/sight/p0/1607/7c/7cda8b6782dabd80b4.img.jpg_600x330_8572a930.jpg"
+               class="gallary-img">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Gallary',
+  data () {
+    return {
+      swiperOptions: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGallaryClick () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow: inherit
+
+.container
+  display: flex
+  flex-direction: column
+  justify-content: center
+  z-index: 99
+  left: 0
+  right: 0
+  bottom: 0
+  top: 0
+  background: #000
+  position: fixed
+  .wrapper
+    height: 0
+    width: 100%
+    padding-bottom: 100%
+    .swiper-pagination
+      color: #fff
+      bottom: -1rem
+    .gallary-img
+      width: 100%
+</style>
--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,14 +2,20 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom" v-for="item of list" :key="item.id">
+      <router-link
+        tag="li"
+        class="item border-bottom"
+        v-for="item of list"
+        :key="item.id"
+        :to="'/detail/' + item.id"
+      >
         <img :src="item.imgUrl" alt="" class="item-img">
         <div class="item-info">
           <p class="item-title">{{item.title}}</p>
           <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
-      </li>
+      </router-link>
     </ul>
   </div>
 </template>
@@ -24,37 +30,37 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-  @import "~styles/mixins.styl"
-  .title
-    margin-top: .2rem
-    line-height: .8rem
-    background: #eee
-    text-index: .2rem
+@import "~styles/mixins.styl"
+.title
+  margin-top: .2rem
+  line-height: .8rem
+  background: #eee
+  text-index: .2rem
 
-  .item
-    overflow: hidden
-    display: flex
+.item
+  overflow: hidden
+  display: flex
+  height: 1.9rem
+  .item-img
+    width: 1.7rem
     height: 1.9rem
-    .item-img
-      width: 1.7rem
-      height: 1.9rem
-      padding: .1rem
-    .item-info
-      flex: 1
-      min-width: 0
-      padding: .1rem
-      .item-title
-        ellipsis()
-        line-height: .54rem
-        font-size: .32rem
-      .item-desc
-        ellipsis()
-        line-height: .4rem
-        color: #616161
-      .item-button
-        color: #fff
-        margin-top: .2rem
-        background: #ff9300
-        padding: 1 .1rem
-        border-radius: .06rem
+    padding: .1rem
+  .item-info
+    flex: 1
+    min-width: 0
+    padding: .1rem
+    .item-title
+      ellipsis()
+      line-height: .54rem
+      font-size: .32rem
+    .item-desc
+      ellipsis()
+      line-height: .4rem
+      color: #616161
+    .item-button
+      color: #fff
+      margin-top: .2rem
+      background: #ff9300
+      padding: 1 .1rem
+      border-radius: .06rem
 </style>
